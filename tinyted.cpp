c36236424ba253cdce135414511556243388c2eb
Anderson S <anderson.schroeder0317@gmail.com>
2024-07-20 23:13:55 -0500

    scale GUI to window size

--- tinyted.cpp
+++ tinyted.cpp
@@ -3,12 +3,38 @@
 #include <unistd.h>
 #include <termios.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
 
 #define K_CTRL(k) ((k) & 0x1f)
+// Responsible for managing visual aspects of terminal
+namespace TerminalGUI {
+	static void wipeScreen();
+	static void resetCursor();
+	static void reset();
+	static void drawRows();
+	static void draw();
+	static int getWindowSize();
+	static int getCursorPosition();
+	static void initGUI();
+}
 
-int rows = 24;
 
-namespace GlobalData {
+// Responsible for handling/implementing errors
+namespace ErrorMgr {
+	static void err(const char *s);
+};
+
+
+// Responsible for managing meta state of Terminal/program
+namespace StateMgr {
+	static void exitRaw();
+	static void enterRaw();
+};
+
+namespace globalConfig {
+	static int sRow;
+	static int sCol;
 	static struct termios tty;
 };
 
@@ -29,8 +55,9 @@ namespace TerminalGUI {
 	}
 
 	static void drawRows() {
-		for (int r = 0; r < rows; r++) {
-			write(STDOUT_FILENO, "~\r\n", 3);
+		for (int r = 0; r < globalConfig::sRow; r++) {
+			std::string s = r < globalConfig::sRow -1 ? "~\r\n" : "~";
+			write(STDOUT_FILENO, s.c_str(), s.size());
 		}
 	}
 
@@ -38,6 +65,42 @@ namespace TerminalGUI {
 		reset();
 		drawRows();
 		resetCursor();
+	}
+
+	static int getWindowSize() {
+		struct winsize w;
+
+		char c;
+		// If ioctl doesnt work use escape sequences to find rows/cols
+		if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1 || w.ws_col == 0) {
+			if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+			return getCursorPosition();
+		}
+		globalConfig::sRow = w.ws_row;
+		globalConfig::sCol = w.ws_col;
+		return 0;
+	}
+
+	static int getCursorPosition() {
+		char buf[32];
+		uint32_t i;
+
+		if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+		for (i = 0; i < sizeof(buf) - 1; i++) {
+			if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+			if (buf[i] == 'R') break;
+		}
+
+		buf[i] = '\0';
+
+		if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+		if (sscanf(&buf[2], "%d;%d", globalConfig::sRow, globalConfig::sCol) != 2) return -1;
+
+		return 0;
+	}
+
+	static void initGUI() {
+		if (getWindowSize() < 0) ErrorMgr::err("getWindowSize");
 	}
 }
 
@@ -56,21 +119,21 @@ namespace ErrorMgr {
 // Responsible for managing meta state of Terminal/program
 namespace StateMgr {
 	static void exitRaw() {
-		int ret = tcsetattr(STDIN_FILENO, TCSANOW, &GlobalData::tty);
+		int ret = tcsetattr(STDIN_FILENO, TCSANOW, &globalConfig::tty);
 		if (ret < 0) {
 			ErrorMgr::err("tcsetattr");
 		}
 	}
 
 	static void enterRaw() {
-		int ret = tcgetattr(STDIN_FILENO, &GlobalData::tty);
+		int ret = tcgetattr(STDIN_FILENO, &globalConfig::tty);
 		if (ret < 0) {
 			ErrorMgr::err("tcgetattr");
 		}
 
 		atexit(exitRaw);
 
-		struct termios copy = GlobalData::tty; 
+		struct termios copy = globalConfig::tty; 
 		copy.c_iflag &= ~(ICRNL | IXON | INPCK | BRKINT | ISTRIP); // Turn off ctrl-s/q and carriage return conversions.
 		copy.c_oflag &= ~(OPOST); // Turn off output processing.
 		copy.c_cflag |= (CS8); // 8 bit bytes
@@ -89,6 +152,7 @@ class Editor {
 	int r;
 
 	const char readKey() {
+		char c;
 		while (r = read(STDIN_FILENO, &c, sizeof(c)) != 1) {
 			if (r < 0 && errno != EAGAIN) {
 				ErrorMgr::err("read");
@@ -119,6 +183,7 @@ public:
 
 int main(){
 	StateMgr::enterRaw();
+	TerminalGUI::initGUI();
 	Editor p;
 
 	while(1) {
